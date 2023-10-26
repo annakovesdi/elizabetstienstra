047805a4492005c954445f0898ddceaed375c527
annakovesdi <annakovesdi@gmail.com>
2023-10-26 11:33:49 +0000

    urls.py info app

---
+++ info/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+     path('news/', views.news, name='news'),
+     path('texts/', views.texts, name='texts'),
+     path('info-management/', views.info_management,
+          name='info_management'),
+     path('add-info/', views.add_info, name='add_info'),
+     path('info-management-detail/', views.info_management_detail,
+          name='info_management_detail'),
+     path('edit-info/<info_id>/', views.edit_info,
+          name='edit_info'),
+     path('delete-info/<info_id>/', views.delete_info,
+          name='delete_info'),
+]
