860caede7b7e1d9d2bbf82378eafb0652a98d0ea
annakovesdi <annakovesdi@gmail.com>
2023-10-26 09:25:34 +0000

    urls.py project

--- elizabetstienstra/urls.py
+++ elizabetstienstra/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
+from django.views.static import serve
+
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
+    path("", include("home.urls")),
+    path("about/", include("about.urls")),
+    path("cv/", include("cv.urls")),
+    path("news/", include("news.urls")),
+    path("oeuvre/", include("oeuvre.urls")),
+    path("text/", include("text.urls")),
     path('admin/', admin.site.urls),
-]
+    path('sitemap.xml', sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
