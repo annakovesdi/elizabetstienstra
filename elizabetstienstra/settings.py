5f431c015cfe254ea9bd899bf0486ff6fa09f616
annakovesdi <annakovesdi@gmail.com>
2023-10-26 09:21:05 +0000

    update settings.py

--- elizabetstienstra/settings.py
+++ elizabetstienstra/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-y*(lmar0d37%*%0rwcxm)83-o!3q9$8cv-ro))sj-k6xdbh#al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'annakovesdi-elizabetsti-44tjhk5bzyq.ws-eu105.gitpod.io/']
+CSRF_TRUSTED_ORIGINS = []
 
 
 # Application definition
@@ -37,24 +39,91 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'home',
+    'oeuvre',
+    'about',
+    'news',
+    'text',
+    'cv',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'sweetify',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
+]
+
+# Security 
+
+SECURE_BROWSER_XSS_FILTER = True
+# SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000 # work up to 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_REFERRER_POLICY = 'same-origin'
+
+# CSP Securty Headers
+
+CSP_DEFAULT_SRC = ["'self'", ]
+CSP_STYLE_SRC = [
+    "'self'", 
+    "https://cdn.jsdelivr.net/", 
+    "https://fonts.googleapis.com/", 
+    "'unsafe-inline'",
+]
+CSP_SCRIPT_SRC = [
+    "'self'", 
+    # 'https://elizabetstienstra.com/',
+]
+CSP_INCLUDE_NONCE_IN = [
+    'script-src',
+    'script-src-elem'
+]
+CSP_CONNECT_SRC = [
+    "'self'",
+    "https://ka-f.fontawesome.com/"
+]
+CSP_IMG_SRC = [
+    "'self'", 
+    'data:',
+    # 'https://res.cloudinary.com/',
+]
+CSP_FRAME_SRC = ["'self'", ]
+CSP_MEDIA_SRC = ["'self'", ]
+CSP_MANIFEST_SRC = ["'self'", ]
+CSP_FONT_SRC = [
+    "'self'", 
+    "https://fonts.gstatic.com/", 
+    "https://fonts.googleapis.com/", 
+    "https://ka-f.fontawesome.com/"
 ]
 
 ROOT_URLCONF = 'elizabetstienstra.urls'
 
+SITE_ID = 1
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,8 +132,22 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
+]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    # 'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'elizabetstienstra.wsgi.application'
@@ -73,12 +156,17 @@ WSGI_APPLICATION = 'elizabetstienstra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -115,7 +203,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
