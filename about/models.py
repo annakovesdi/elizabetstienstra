814bf6fcf3a1a91f32ce21f88c7ea344d89818de
annakovesdi <annakovesdi@gmail.com>
2023-11-30 13:31:29 +0000

    about app adjustments

--- about/models.py
+++ about/models.py
@@ -7,8 +7,6 @@ class About(models.Model):
     title = models.CharField(max_length=254, null=False, blank=False)
     description = RichTextField(null=False, blank=False)
     image = models.ImageField(null=False, blank=False)
-    image2 = models.ImageField(null=True, blank=True)
-    image3 = models.ImageField(null=True, blank=True)
     contact = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
