bb73e5f589f6cc07c4460b630b7c23397140caaf
AdministratorBea <148860501+AdministratorBea@users.noreply.github.com>
2024-04-03 13:50:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --no-input
 python manage.py makemigrations && python manage.py migrate
